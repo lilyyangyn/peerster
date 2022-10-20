2f48adc6b92ac0a355ac8acaf5bc265fa83a3ea7
lilyyangyn <yyn605@outlook.com>
2022-10-20 17:21:43 +0200

    refactor code

--- peer/impl/mod.go
+++ peer/impl/mod.go
@@ -3,8 +3,6 @@ package impl
 import (
 	"context"
 	"encoding/json"
-	"math/rand"
-	"sync"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -57,124 +55,6 @@ type node struct {
 	antiEntropyStopSig context.CancelFunc
 }
 
-// SafeRoutingTable implements a thread-safe routing table
-type SafeRoutingTable struct {
-	*sync.RWMutex
-	table peer.RoutingTable
-}
-
-func (t SafeRoutingTable) add(key string, val string) {
-	t.Lock()
-	defer t.Unlock()
-	t.table[key] = val
-}
-func (t SafeRoutingTable) remove(key string) {
-	t.Lock()
-	defer t.Unlock()
-	delete(t.table, key)
-}
-func (t SafeRoutingTable) get(key string) (string, bool) {
-	t.RLock()
-	val, ok := t.table[key]
-	t.RUnlock()
-	return val, ok
-}
-func (t SafeRoutingTable) getAll() peer.RoutingTable {
-	routingTable := peer.RoutingTable{}
-	t.RLock()
-	for key, value := range t.table {
-		routingTable[key] = value
-	}
-	t.RUnlock()
-	return routingTable
-}
-
-func NewSafeRoutingTable(addr string) *SafeRoutingTable {
-	rt := SafeRoutingTable{&sync.RWMutex{}, peer.RoutingTable{}}
-	rt.add(addr, addr)
-	return &rt
-}
-
-type RumorsTable map[string][]types.Rumor
-
-// SafeRoutingTable implements a thread-safe routing table
-type SafeRumorsTable struct {
-	*sync.RWMutex
-	table RumorsTable
-}
-
-func (t SafeRumorsTable) add(rumor types.Rumor) bool {
-	t.Lock()
-	defer t.Unlock()
-
-	if uint(len(t.table[rumor.Origin]))+1 != rumor.Sequence {
-		return false
-	}
-	t.table[rumor.Origin] = append(t.table[rumor.Origin], rumor)
-	return true
-}
-func (t SafeRumorsTable) getExpectedSeq(key string) uint {
-	t.RLock()
-	rumors := t.table[key]
-	t.RUnlock()
-	return uint(len(rumors)) + 1
-}
-func (t SafeRumorsTable) getRumorsFrom(key string, seqId uint) ([]types.Rumor, bool) {
-	rumors := []types.Rumor{}
-	t.RLock()
-	length := uint(len(t.table[key]))
-	if seqId > length {
-		return rumors, false
-	}
-	for i := seqId - 1; i < length; i++ {
-		rumors = append(rumors, t.table[key][i])
-	}
-	t.RUnlock()
-	return rumors, true
-}
-func (t SafeRumorsTable) getStatus() map[string]uint {
-	statusTable := make(map[string]uint)
-	t.RLock()
-	for key, value := range t.table {
-		statusTable[key] = uint(len(value))
-	}
-	t.RUnlock()
-	return statusTable
-}
-func NewSafeRumorsTable() *SafeRumorsTable {
-	rt := SafeRumorsTable{&sync.RWMutex{}, RumorsTable{}}
-	return &rt
-}
-
-type TimerTable map[string]chan struct{}
-
-// TimerController implements a thread-safe table for timer
-type TimerController struct {
-	*sync.RWMutex
-	table TimerTable
-}
-
-func (t TimerController) add(pktId string, done chan struct{}) {
-	t.Lock()
-	defer t.Unlock()
-	t.table[pktId] = done
-}
-func (t TimerController) remove(key string) {
-	t.Lock()
-	defer t.Unlock()
-	delete(t.table, key)
-}
-func (t TimerController) get(key string) (chan struct{}, bool) {
-	t.RLock()
-	val, ok := t.table[key]
-	t.RUnlock()
-	return val, ok
-}
-func NewTimeController() *TimerController {
-	rt := TimerController{&sync.RWMutex{}, TimerTable{}}
-	return &rt
-}
-
 // Start implements peer.Service
 func (n *node) Start() error {
 	//start a new loop to listen to the message (non-blocking)
@@ -199,10 +79,13 @@ func (n *node) Start() error {
 		}
 	}(ctx)
 
-	n.HeartBeatMecahnism(n.conf.HeartbeatInterval)
-	n.AntiEntropyMechanism(n.conf.AntiEntropyInterval)
+	err := n.HeartBeatMecahnism(n.conf.HeartbeatInterval)
+	if err != nil {
+		return err
+	}
+	err = n.AntiEntropyMechanism(n.conf.AntiEntropyInterval)
 	// return once ready to use
-	return nil
+	return err
 }
 
 // Stop implements peer.Service
@@ -297,109 +180,6 @@ func (n *node) SetRoutingEntry(origin, relayAddr string) {
 	n.routingTable.add(origin, relayAddr)
 }
 
-// GetRoutingInfo gets routing information from routing table or error if entry not exists
-func (n *node) GetRoutingInfo(dst string) (string, error) {
-	nextHop, ok := n.routingTable.get(dst)
-	if !ok {
-		// no routing information. Just drop the packet
-		return "", xerrors.Errorf("No routing information to %s", dst)
-	}
-	return nextHop, nil
-}
-
-// GetRandomNeighbor randomly returns a neighbor
-func (n *node) GetRandomNeighbor(exclude string) (string, bool) {
-	n.routingTable.RLock()
-	neighbors := []string{}
-	for key, val := range n.routingTable.table {
-		if key == n.conf.Socket.GetAddress() {
-			continue
-		}
-		if key == exclude {
-			continue
-		}
-		if key == val {
-			neighbors = append(neighbors, key)
-		}
-	}
-	n.routingTable.RUnlock()
-	if len(neighbors) == 0 {
-		return "", false
-	} else {
-		return neighbors[rand.Intn(len(neighbors))], true
-	}
-}
-
-// CreateRumor creates a new rumor with expected seqId and add it to rumorsTable
-func (n *node) CreateRumor(msg *transport.Message) types.Rumor {
-	n.rumorsTable.Lock()
-	defer n.rumorsTable.Unlock()
-	expectedSeq := uint(len(n.rumorsTable.table[n.conf.Socket.GetAddress()])) + 1
-	rumor := types.Rumor{
-		Origin:   n.conf.Socket.GetAddress(),
-		Sequence: expectedSeq,
-		Msg:      msg,
-	}
-	n.rumorsTable.table[rumor.Origin] = append(n.rumorsTable.table[rumor.Origin], rumor)
-	return rumor
-}
-
-// CreateMsg creates a new transport message for the given payload
-func (n *node) CreateMsg(payload types.Message) (transport.Message, error) {
-	data, err := json.Marshal(&payload)
-	if err != nil {
-		return transport.Message{}, err
-	}
-	msg := transport.Message{Type: payload.Name(), Payload: data}
-	return msg, nil
-}
-
-// RegisterTimer registers a timer to resend the packet after a certain period
-func (n *node) RegisterTimer(pkt *transport.Packet, duration time.Duration) {
-	if duration == 0 {
-		// no timer will be set
-		return
-	}
-	done := make(chan struct{})
-	timer := time.NewTimer(duration)
-	go func() {
-		select {
-		case <-done:
-		case <-timer.C:
-			close(done)
-			n.timerController.remove(pkt.Header.PacketID)
-			neighbor, ok := n.GetRandomNeighbor(pkt.Header.Destination)
-			if !ok {
-				return
-			}
-			header := transport.NewHeader(
-				n.conf.Socket.GetAddress(),
-				n.conf.Socket.GetAddress(),
-				neighbor,
-				0)
-			msg := pkt.Msg.Copy()
-			myPkt := transport.Packet{Header: &header, Msg: &msg}
-			n.RegisterTimer(&myPkt, n.conf.AckTimeout)
-			err := n.conf.Socket.Send(neighbor, myPkt, WriteTimeout)
-			if err != nil {
-				return
-			}
-		}
-	}()
-	n.timerController.add(pkt.Header.PacketID, done)
-}
-
-// CancelTimer cancels the registed timer based on packetID
-func (n *node) CancelTimer(pktId string) error {
-	done, ok := n.timerController.get(pktId)
-	if ok {
-		<-done
-		close(done)
-		n.timerController.remove(pktId)
-	}
-	return nil
-}
-
 // ProcessPkt processes packet received
 func (n *node) ProcessPkt(pkt transport.Packet) error {
 	pktDst := pkt.Header.Destination
@@ -425,6 +205,7 @@ func (n *node) ProcessPkt(pkt transport.Packet) error {
 	return nil
 }
 
+// HeartBeatMecahnism implements heartbeat mechanism to periodically notify self
 func (n *node) HeartBeatMecahnism(interval time.Duration) error {
 	if interval == 0 {
 		// the heartbeat mechanism must not be activated
@@ -455,6 +236,7 @@ func (n *node) HeartBeatMecahnism(interval time.Duration) error {
 	return nil
 }
 
+// AntiEntropyMechanism implements anti-entropy mechanism for gossip sync
 func (n *node) AntiEntropyMechanism(interval time.Duration) error {
 	if interval == 0 {
 		// the anti-entropy mechanism must not be activated
@@ -485,46 +267,6 @@ func (n *node) AntiEntropyMechanism(interval time.Duration) error {
 	return nil
 }
 
-// SendAckMessage sends an ACK packet to neighbor
-func (n *node) SendAckMessage(dst string, ACKPktID string) error {
-	payload := types.AckMessage{AckedPacketID: ACKPktID, Status: types.StatusMessage(n.rumorsTable.getStatus())}
-	msg, err := n.CreateMsg(payload)
-	if err != nil {
-		return err
-	}
-	err = n.SendToNeighbor(dst, msg)
-	return err
-}
-
-// SendStatusMessage sends an status packet to neighbor
-func (n *node) SendStatusMessage(dst string) error {
-	payload := types.StatusMessage(n.rumorsTable.getStatus())
-	msg, err := n.CreateMsg(payload)
-	if err != nil {
-		return err
-	}
-	err = n.SendToNeighbor(dst, msg)
-	return err
-}
-
-// SendRumorsMessage unicasts an rumors packet
-func (n *node) SendRumorsMessage(dst string, rumors *[]types.Rumor) error {
-	payload := types.RumorsMessage{Rumors: *rumors}
-	msg, err := n.CreateMsg(payload)
-	if err != nil {
-		return err
-	}
-	header := transport.NewHeader(
-		n.conf.Socket.GetAddress(),
-		n.conf.Socket.GetAddress(),
-		dst,
-		0)
-	pkt := transport.Packet{Header: &header, Msg: &msg}
-	n.RegisterTimer(&pkt, n.conf.AckTimeout)
-	err = n.conf.Socket.Send(dst, pkt, WriteTimeout)
-	return err
-}
-
 // ProcessChatMessage is a callback function to handle received chat message
 func (n *node) ProcessChatMessage(msg types.Message, pkt transport.Packet) error {
 	// cast the message to its actual type. You assume it is the right type.
@@ -533,7 +275,10 @@ func (n *node) ProcessChatMessage(msg types.Message, pkt transport.Packet) error
 		return xerrors.Errorf("wrong type: %T", msg)
 	}
 
-	log.Info().Msgf("%s received a chat message from: %s. Msg: %s", n.conf.Socket.GetAddress(), pkt.Header.Source, chatMsg.Message)
+	log.Info().Msgf("%s received a chat message from: %s. Msg: %s",
+		n.conf.Socket.GetAddress(),
+		pkt.Header.Source,
+		chatMsg.Message)
 	return nil
 }
 
@@ -567,13 +312,7 @@ func (n *node) ProcessRumorsMsg(msg types.Message, pkt transport.Packet) error {
 	// process RumorsMsg
 	for _, rumor := range rumorsMsg.Rumors {
 		// ignore unexpected rumor. Otherwise, update table
-		// if n.conf.Socket.GetAddress() == "127.0.0.1:1" {
-		// 	fmt.Println(rumor.Origin, rumor.Sequence)
-		// }
 		if n.rumorsTable.add(rumor) {
-			// if n.conf.Socket.GetAddress() == "127.0.0.1:1" {
-			// 	fmt.Println("valid")
-			// }
 			toNeighbor = true
 			// update routing table
 			oldRelay, ok := n.routingTable.get(rumor.Origin)
@@ -614,29 +353,7 @@ func (n *node) ProcessStatusMsg(msg types.Message, pkt transport.Packet) error {
 	if !ok {
 		return xerrors.Errorf("wrong type: %T", msg)
 	}
-	rumors := []types.Rumor{}
-	catchUp := false
-	for key, val := range *statusMsg {
-		if n.rumorsTable.getExpectedSeq(key)-1 < val {
-			// the remote peer has new rumors
-			catchUp = true
-		} else if n.rumorsTable.getExpectedSeq(key)-1 > val {
-			// current noed has new rumors
-			newRumors, ok := n.rumorsTable.getRumorsFrom(key, val+1)
-			if ok {
-				rumors = append(rumors, newRumors...)
-			}
-		}
-	}
-	myStatus := n.rumorsTable.getStatus()
-	for key, _ := range *&myStatus {
-		_, ok = (*statusMsg)[key]
-		if !ok {
-			// in case the remote peer has no entire entry
-			newRumors, _ := n.rumorsTable.getRumorsFrom(key, 1)
-			rumors = append(rumors, newRumors...)
-		}
-	}
+	rumors, catchUp := n.CheckSyncStatus(statusMsg)
 
 	if catchUp {
 		// send a status message to the remote peer
@@ -644,7 +361,7 @@ func (n *node) ProcessStatusMsg(msg types.Message, pkt transport.Packet) error {
 		return err
 	}
 	if len(rumors) > 0 {
-		// send all the missing Rumors (increasing seqId) in a single RumorsMessage
+		// send all the missing Rumors (increasing seqID) in a single RumorsMessage
 		payload := types.RumorsMessage{Rumors: rumors}
 		msg, err := n.CreateMsg(payload)
 		if err != nil {
@@ -656,20 +373,7 @@ func (n *node) ProcessStatusMsg(msg types.Message, pkt transport.Packet) error {
 	}
 	if !catchUp && len(rumors) == 0 {
 		// Both peers have the same view. ContinueMongering
-		chance := rand.Float64()
-		if chance > n.conf.ContinueMongering {
-			// continue only with a certain probability
-			return nil
-		}
-		randomNeighbor, ok := n.GetRandomNeighbor(pkt.Header.Source)
-		if ok {
-			payload := types.StatusMessage(myStatus)
-			msg, err := n.CreateMsg(payload)
-			if err != nil {
-				return err
-			}
-			n.SendToNeighbor(randomNeighbor, msg)
-		}
+		return n.ContinueMongering(pkt.Header.Source)
 	}
 	return nil
 }
---
+++ peer/impl/utils.go
@@ -0,0 +1,323 @@
+package impl
+
+import (
+	"encoding/json"
+	"math/rand"
+	"sync"
+	"time"
+
+	"go.dedis.ch/cs438/peer"
+	"go.dedis.ch/cs438/transport"
+	"go.dedis.ch/cs438/types"
+	"golang.org/x/xerrors"
+)
+
+/** Safe Structure **/
+
+// SafeRoutingTable implements a thread-safe routing table
+type SafeRoutingTable struct {
+	*sync.RWMutex
+	table peer.RoutingTable
+}
+
+func (t SafeRoutingTable) add(key string, val string) {
+	t.Lock()
+	defer t.Unlock()
+	t.table[key] = val
+}
+func (t SafeRoutingTable) remove(key string) {
+	t.Lock()
+	defer t.Unlock()
+	delete(t.table, key)
+}
+func (t SafeRoutingTable) get(key string) (string, bool) {
+	t.RLock()
+	val, ok := t.table[key]
+	t.RUnlock()
+	return val, ok
+}
+func (t SafeRoutingTable) getAll() peer.RoutingTable {
+	routingTable := peer.RoutingTable{}
+	t.RLock()
+	for key, value := range t.table {
+		routingTable[key] = value
+	}
+	t.RUnlock()
+	return routingTable
+}
+
+func NewSafeRoutingTable(addr string) *SafeRoutingTable {
+	rt := SafeRoutingTable{&sync.RWMutex{}, peer.RoutingTable{}}
+	rt.add(addr, addr)
+	return &rt
+}
+
+type RumorsTable map[string][]types.Rumor
+
+// SafeRoutingTable implements a thread-safe routing table
+type SafeRumorsTable struct {
+	*sync.RWMutex
+	table RumorsTable
+}
+
+func (t SafeRumorsTable) add(rumor types.Rumor) bool {
+	t.Lock()
+	defer t.Unlock()
+
+	if uint(len(t.table[rumor.Origin]))+1 != rumor.Sequence {
+		return false
+	}
+	t.table[rumor.Origin] = append(t.table[rumor.Origin], rumor)
+	return true
+}
+func (t SafeRumorsTable) getExpectedSeq(key string) uint {
+	t.RLock()
+	rumors := t.table[key]
+	t.RUnlock()
+	return uint(len(rumors)) + 1
+}
+func (t SafeRumorsTable) getRumorsFrom(key string, seqID uint) ([]types.Rumor, bool) {
+	rumors := []types.Rumor{}
+	t.RLock()
+	length := uint(len(t.table[key]))
+	if seqID > length {
+		return rumors, false
+	}
+	for i := seqID - 1; i < length; i++ {
+		rumors = append(rumors, t.table[key][i])
+	}
+	t.RUnlock()
+	return rumors, true
+}
+func (t SafeRumorsTable) getStatus() map[string]uint {
+	statusTable := make(map[string]uint)
+	t.RLock()
+	for key, value := range t.table {
+		statusTable[key] = uint(len(value))
+	}
+	t.RUnlock()
+	return statusTable
+}
+func NewSafeRumorsTable() *SafeRumorsTable {
+	rt := SafeRumorsTable{&sync.RWMutex{}, RumorsTable{}}
+	return &rt
+}
+
+type TimerTable map[string]chan struct{}
+
+// TimerController implements a thread-safe table for timer
+type TimerController struct {
+	*sync.RWMutex
+	table TimerTable
+}
+
+func (t TimerController) add(pktID string, done chan struct{}) {
+	t.Lock()
+	defer t.Unlock()
+	t.table[pktID] = done
+}
+func (t TimerController) remove(key string) {
+	t.Lock()
+	defer t.Unlock()
+	delete(t.table, key)
+}
+func (t TimerController) get(key string) (chan struct{}, bool) {
+	t.RLock()
+	val, ok := t.table[key]
+	t.RUnlock()
+	return val, ok
+}
+func NewTimeController() *TimerController {
+	rt := TimerController{&sync.RWMutex{}, TimerTable{}}
+	return &rt
+}
+
+/** Private Helpfer Functions **/
+
+// GetRoutingInfo gets routing information from routing table or error if entry not exists
+func (n *node) GetRoutingInfo(dst string) (string, error) {
+	nextHop, ok := n.routingTable.get(dst)
+	if !ok {
+		// no routing information. Just drop the packet
+		return "", xerrors.Errorf("No routing information to %s", dst)
+	}
+	return nextHop, nil
+}
+
+// GetRandomNeighbor randomly returns a neighbor
+func (n *node) GetRandomNeighbor(exclude string) (string, bool) {
+	n.routingTable.RLock()
+	neighbors := []string{}
+	for key, val := range n.routingTable.table {
+		if key == n.conf.Socket.GetAddress() {
+			continue
+		}
+		if key == exclude {
+			continue
+		}
+		if key == val {
+			neighbors = append(neighbors, key)
+		}
+	}
+	n.routingTable.RUnlock()
+	if len(neighbors) == 0 {
+		return "", false
+	}
+	return neighbors[rand.Intn(len(neighbors))], true
+}
+
+// CreateRumor creates a new rumor with expected seqID and add it to rumorsTable
+func (n *node) CreateRumor(msg *transport.Message) types.Rumor {
+	n.rumorsTable.Lock()
+	defer n.rumorsTable.Unlock()
+	expectedSeq := uint(len(n.rumorsTable.table[n.conf.Socket.GetAddress()])) + 1
+	rumor := types.Rumor{
+		Origin:   n.conf.Socket.GetAddress(),
+		Sequence: expectedSeq,
+		Msg:      msg,
+	}
+	n.rumorsTable.table[rumor.Origin] = append(n.rumorsTable.table[rumor.Origin], rumor)
+	return rumor
+}
+
+// CreateMsg creates a new transport message for the given payload
+func (n *node) CreateMsg(payload types.Message) (transport.Message, error) {
+	data, err := json.Marshal(&payload)
+	if err != nil {
+		return transport.Message{}, err
+	}
+	msg := transport.Message{Type: payload.Name(), Payload: data}
+	return msg, nil
+}
+
+// RegisterTimer registers a timer to resend the packet after a certain period
+func (n *node) RegisterTimer(pkt *transport.Packet, duration time.Duration) {
+	if duration == 0 {
+		// no timer will be set
+		return
+	}
+	done := make(chan struct{})
+	timer := time.NewTimer(duration)
+	go func() {
+		select {
+		case <-done:
+		case <-timer.C:
+			close(done)
+			n.timerController.remove(pkt.Header.PacketID)
+			neighbor, ok := n.GetRandomNeighbor(pkt.Header.Destination)
+			if !ok {
+				return
+			}
+			header := transport.NewHeader(
+				n.conf.Socket.GetAddress(),
+				n.conf.Socket.GetAddress(),
+				neighbor,
+				0)
+			msg := pkt.Msg.Copy()
+			myPkt := transport.Packet{Header: &header, Msg: &msg}
+			n.RegisterTimer(&myPkt, n.conf.AckTimeout)
+			err := n.conf.Socket.Send(neighbor, myPkt, WriteTimeout)
+			if err != nil {
+				return
+			}
+		}
+	}()
+	n.timerController.add(pkt.Header.PacketID, done)
+}
+
+// CancelTimer cancels the registed timer based on packetID
+func (n *node) CancelTimer(pktID string) {
+	done, ok := n.timerController.get(pktID)
+	if ok {
+		<-done
+		close(done)
+		n.timerController.remove(pktID)
+	}
+}
+
+// SendAckMessage sends an ACK packet to neighbor
+func (n *node) SendAckMessage(dst string, ACKPktID string) error {
+	payload := types.AckMessage{AckedPacketID: ACKPktID, Status: types.StatusMessage(n.rumorsTable.getStatus())}
+	msg, err := n.CreateMsg(payload)
+	if err != nil {
+		return err
+	}
+	err = n.SendToNeighbor(dst, msg)
+	return err
+}
+
+// SendStatusMessage sends an status packet to neighbor
+func (n *node) SendStatusMessage(dst string) error {
+	payload := types.StatusMessage(n.rumorsTable.getStatus())
+	msg, err := n.CreateMsg(payload)
+	if err != nil {
+		return err
+	}
+	err = n.SendToNeighbor(dst, msg)
+	return err
+}
+
+// SendRumorsMessage unicasts an rumors packet
+func (n *node) SendRumorsMessage(dst string, rumors *[]types.Rumor) error {
+	payload := types.RumorsMessage{Rumors: *rumors}
+	msg, err := n.CreateMsg(payload)
+	if err != nil {
+		return err
+	}
+	header := transport.NewHeader(
+		n.conf.Socket.GetAddress(),
+		n.conf.Socket.GetAddress(),
+		dst,
+		0)
+	pkt := transport.Packet{Header: &header, Msg: &msg}
+	n.RegisterTimer(&pkt, n.conf.AckTimeout)
+	err = n.conf.Socket.Send(dst, pkt, WriteTimeout)
+	return err
+}
+
+// CheckSyncStatus compares the node status with the statusMessage for furthur syncing
+func (n *node) CheckSyncStatus(statusMsg *types.StatusMessage) ([]types.Rumor, bool) {
+	rumors := []types.Rumor{}
+	catchUp := false
+	for key, val := range *statusMsg {
+		if n.rumorsTable.getExpectedSeq(key)-1 < val {
+			// the remote peer has new rumors
+			catchUp = true
+		} else if n.rumorsTable.getExpectedSeq(key)-1 > val {
+			// current noed has new rumors
+			newRumors, ok := n.rumorsTable.getRumorsFrom(key, val+1)
+			if ok {
+				rumors = append(rumors, newRumors...)
+			}
+		}
+	}
+	myStatus := n.rumorsTable.getStatus()
+	for key := range myStatus {
+		_, ok := (*statusMsg)[key]
+		if !ok {
+			// in case the remote peer has no entire entry
+			newRumors, _ := n.rumorsTable.getRumorsFrom(key, 1)
+			rumors = append(rumors, newRumors...)
+		}
+	}
+	return rumors, catchUp
+}
+
+// ContinueMongering implements the continue mongering mechanism
+func (n *node) ContinueMongering(pktOrigin string) error {
+	chance := rand.Float64()
+	if chance > n.conf.ContinueMongering {
+		// continue only with a certain probability
+		return nil
+	}
+	randomNeighbor, ok := n.GetRandomNeighbor(pktOrigin)
+	if ok {
+		payload := types.StatusMessage(n.rumorsTable.getStatus())
+		msg, err := n.CreateMsg(payload)
+		if err != nil {
+			return err
+		}
+		return n.SendToNeighbor(randomNeighbor, msg)
+	}
+	return nil
+}
