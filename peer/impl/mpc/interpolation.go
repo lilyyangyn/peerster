136f1cc7754c90bc06f0ef60078021e7cee6f49b
vesperlou <Vesper_Lou@outlook.com>
2022-12-15 21:27:35 +0100

    Lagrange interpolation (float) complete and tested - with comment

--- peer/impl/mpc/interpolation.go
+++ peer/impl/mpc/interpolation.go
@@ -1,6 +1,12 @@
 package mpc
 
+// float version of Lagrange Interpolation
+// assume no OVERFLOW !!
+// TO DO: GF(2^8) version?
 func (m *MPCModule) lagrangeInterpolation(values []int, xcoord []int) (result int) {
+	// length of two input arrays must be identical
+	// equal to number of nodes
+
 	var floatResult float64 = 0
 	for i, y := range values {
 		var w float64 = 1
