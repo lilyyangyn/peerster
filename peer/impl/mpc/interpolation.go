873206d6a67bb689e49824a3d190e1d3f4744810
vesperlou <Vesper_Lou@outlook.com>
2022-12-15 21:21:17 +0100

    Lagrange interpolation (float) complete and tested

--- peer/impl/mpc/interpolation.go
+++ peer/impl/mpc/interpolation.go
@@ -1,5 +1,18 @@
 package mpc
 
-func (m *MPCModule) lagrangeInterpolation(values []int, peers []int) (result int) {
+func (m *MPCModule) lagrangeInterpolation(values []int, xcoord []int) (result int) {
+	var floatResult float64 = 0
+	for i, y := range values {
+		var w float64 = 1
+		for j, x := range xcoord {
+			if i == j {
+				continue
+			}
+			tmp := float64(x) / float64(x-xcoord[i])
+			w = w * tmp
+		}
+		floatResult = floatResult + w*float64(y)
+	}
+	result = int(floatResult)
 	return
 }
